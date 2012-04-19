222e18f729ba60c4b326571bbd7d604143fd4afd
Long Sun <sun.long@rea-group.com>
2012-04-19 11:47:02 +0800

    add TODO for Lei & Shengtao for later refactoring

--- src/BehaviorRepository.m
+++ src/BehaviorRepository.m
@@ -13,38 +13,38 @@ static NSDictionary *categoryNamesDict = nil;
 + (void)initialize {
   if (!categoryNamesDict) {
     categoryNamesDict = DICT(
-    [NSNumber numberWithInt:1], @"准一功",
-    [NSNumber numberWithInt:3], @"准三功",
-    [NSNumber numberWithInt:5], @"准五功",
-    [NSNumber numberWithInt:10], @"准十功",
-    [NSNumber numberWithInt:30], @"准三十功",
-    [NSNumber numberWithInt:50], @"准五十功",
-    [NSNumber numberWithInt:100], @"准百功",
-    [NSNumber numberWithInt:-1], @"准一过",
-    [NSNumber numberWithInt:-3], @"准三过",
-    [NSNumber numberWithInt:-5], @"准五过",
-    [NSNumber numberWithInt:-10], @"准十过",
-    [NSNumber numberWithInt:-30], @"准三十过",
-    [NSNumber numberWithInt:-50], @"准五十过",
-    [NSNumber numberWithInt:-100], @"准百过"
-    );
+                             [NSNumber numberWithInt:1], @"准一功",
+                             [NSNumber numberWithInt:3], @"准三功",
+                             [NSNumber numberWithInt:5], @"准五功",
+                             [NSNumber numberWithInt:10], @"准十功",
+                             [NSNumber numberWithInt:30], @"准三十功",
+                             [NSNumber numberWithInt:50], @"准五十功",
+                             [NSNumber numberWithInt:100], @"准百功",
+                             [NSNumber numberWithInt:-1], @"准一过",
+                             [NSNumber numberWithInt:-3], @"准三过",
+                             [NSNumber numberWithInt:-5], @"准五过",
+                             [NSNumber numberWithInt:-10], @"准十过",
+                             [NSNumber numberWithInt:-30], @"准三十过",
+                             [NSNumber numberWithInt:-50], @"准五十过",
+                             [NSNumber numberWithInt:-100], @"准百过"
+                             );
   }
 }
 
 + (BehaviorRepository *)repositoryFromPredicate:(NSString *)predicate {
   NSManagedObjectContext *context = [NSManagedObjectContext defaultContext];
-
+  
   NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:@"Behavior"];
-
+  
   [request setPredicate:[NSPredicate predicateWithFormat:predicate]];
   [request setSortDescriptors:Array([NSSortDescriptor sortDescriptorWithKey:@"timestamp" ascending:YES])];
-
+  
   __block NSArray *results;
-
+  
   [context performBlockAndWait:^{
     results = [context executeFetchRequest:request error:nil];
   }];
-
+  
   return [[BehaviorRepository alloc] initWithBehaviors:results];
 }
 
@@ -71,18 +71,18 @@ static NSDictionary *categoryNamesDict = nil;
   if (self) {
     NSArray *ranks = [behaviors valueForKeyPath:@"@distinctUnionOfObjects.rank"];
     ranks = [ranks sortedArrayUsingDescriptors:[NSArray arrayWithObject:
-        [NSSortDescriptor sortDescriptorWithKey:@"absInt" ascending:YES]]];
+                                                [NSSortDescriptor sortDescriptorWithKey:@"absInt" ascending:YES]]];
     //TODO: I need NSArray#sortAscending NSArray#reverse
-
+    
     categories_ = [categoryNamesDict objectsForKeys:ranks notFoundMarker:@"未知"];
-
+    
     arrayOfBehaviors_ = [NSMutableArray new];
     for (NSNumber *rank in ranks) {
       NSArray *behaviorsInSameRank = [behaviors filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"rank = %@", rank]];
       [arrayOfBehaviors_ addObject:behaviorsInSameRank];
     }
   }
-
+  
   return self;
 }
 
@@ -103,35 +103,36 @@ static NSDictionary *categoryNamesDict = nil;
 }
 
 - (NSNumber *)totalRank {
-    __block NSInteger result = 0;
-    [arrayOfBehaviors_ each:^(NSArray *behaviors) {
-        [behaviors each:^(id item) {
-           result = result + [self totalRankForBehavior:item]; 
-        }];
+  __block NSInteger result = 0;
+  [arrayOfBehaviors_ each:^(NSArray *behaviors) {
+    [behaviors each:^(id item) {
+      result = result + [self totalRankForBehavior:item]; 
     }];
-    return [NSNumber numberWithInt:result];
+  }];
+  return [NSNumber numberWithInt:result];
 }
 
 - (NSInteger)totalRankForBehavior:(Behavior *)behavior {
-    
-    NSManagedObjectContext *context = [NSManagedObjectContext defaultContext];
-    
-    NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:@"Event"];
-    
-    [request setPredicate:[NSPredicate predicateWithFormat:@"behavior=%@", behavior]];
-    
-    __block NSArray *results = nil;
-    
-    [context performBlockAndWait:^{
-        results = [context executeFetchRequest:request error:nil];
-    }];
-    
-    __block NSInteger totalRank = 0;
-    [results each:^(Event * event) {
-        totalRank = event.countValue * [[behavior rank] intValue];
-    }];
-
-    return totalRank;
+  
+  NSManagedObjectContext *context = [NSManagedObjectContext defaultContext];
+  
+  NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:@"Event"];
+  
+  // TODO: (Lei & Shengtao) use behavior in behavior_list, https://developer.apple.com/library/mac/#documentation/Cocoa/Conceptual/Predicates/Articles/pCreating.html
+  [request setPredicate:[NSPredicate predicateWithFormat:@"behavior=%@", behavior]];
+  
+  __block NSArray *results = nil;
+  
+  [context performBlockAndWait:^{
+    results = [context executeFetchRequest:request error:nil];
+  }];
+  
+  __block NSInteger totalRank = 0;
+  [results each:^(Event * event) {
+    totalRank = event.countValue * [[behavior rank] intValue];
+  }];
+  
+  return totalRank;
 }
 
 
