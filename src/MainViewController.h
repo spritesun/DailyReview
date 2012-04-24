ae7816f6cd053f9020e149fec3ee0a2a96632288
suoqin <kpsesuo@gmail.com>
2012-04-25 02:40:02 +0800

    change color of tab bar item, crazy color adjusting @_@

--- src/MainViewController.h
+++ src/MainViewController.h
@@ -1,3 +1,5 @@
-@interface MainViewController : UITabBarController
+@interface MainViewController : UITabBarController <UITabBarDelegate>
+
+@property (strong, nonatomic) IBOutlet UITabBar *tabBar;
 
 @end
--- src/MainViewController.m
+++ src/MainViewController.m
@@ -2,4 +2,29 @@
 
 @implementation MainViewController
 
+@synthesize tabBar = tabBar_;
+
+#pragma mark - LifeCycles
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  UIImage *image = [UIImage imageNamed:@"bottom-bar-bg.png"];
+  UIImageView *bgView = [[UIImageView alloc] initWithImage:image];
+  bgView.frame = CGRectMake(0, 432, 320, 48);
+  tabBar_.backgroundImage = image;
+  UIImage *transparentImage = [self transparentImage];
+  tabBar_.selectionIndicatorImage = transparentImage;
+}
+
+- (UIImage *)transparentImage {
+  UIGraphicsBeginImageContextWithOptions(CGSizeMake(1, 1), YES, 0.0);
+  UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  return newImage;
+}
+
+- (void)viewDidUnload {
+  [self setTabBar:nil];
+  [super viewDidUnload];
+}
 @end
