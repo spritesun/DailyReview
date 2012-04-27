47578a361c7eb9d0695b0dfc5c83fbe373a34dcd
leizhang <zhanglei728@gmail.com>
2012-04-27 21:05:14 +0800

    add more navigation screen

---
+++ DailyReview/src/MoreController.h
@@ -0,0 +1,13 @@
+//
+//  MoreController.h
+//  DailyReview
+//
+//  Created by Lei Zhang on 4/27/12.
+//  Copyright 2012 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreController : UITableViewController
+
+@end
