dafac06ae02243f2d5ed2dbee482a400b6f278c8
Long Sun <sun.long@rea-group.com>
2012-05-18 19:24:18 +0800

    tweak section header view position

--- DailyReview/src/BehaviorSectionHeaderView.m
+++ DailyReview/src/BehaviorSectionHeaderView.m
@@ -10,23 +10,15 @@
   return [[self alloc] initWithTitle:title];
 }
 
-- (UILabel *)createScoreLabel {
-  UILabel *scoreLabel = [[UILabel alloc] initWithFrame:CGRectMake(200, 0, 120, 30)];  
-  scoreLabel.backgroundColor = [UIColor clearColor];
-  scoreLabel.textColor = [UIColor whiteColor];
-  scoreLabel.font = [UIFont systemFontOfSize:15];
-  return scoreLabel;
-}
-
 - (void)drawBackground {
   UIImage *image = [UIImage imageNamed:@"section-bg.png"];
   UIImageView *bgView = [[UIImageView alloc] initWithImage:image];
-  bgView.frame = CGRectMake(0, 0, 320, 30);
+  bgView.width = SCREEN_WIDTH;
   [self addSubview:bgView];
 }
 
 - (id)initWithTitle:(NSString *)title {
-  self = [self initWithFrame:CGRectMake(0, 0, 320, 30)];
+  self = [self initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 29)];
   if (self) {
     expanded_ = YES;
     [self drawBackground];
@@ -36,7 +28,7 @@
     label.left = 10;
     label.text = title;
     label.textColor = [UIColor blackColor];
-    label.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
+    label.backgroundColor = [UIColor clearColor];
     label.userInteractionEnabled = YES;
     [self addSubview:label];
   }
