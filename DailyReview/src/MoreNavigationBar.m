2115cf79cfc52bf8feb904b292f5986785f5a298
Long Sun <sun.long@rea-group.com>
2012-05-18 21:00:09 +0800

    tweak moreview color

--- DailyReview/src/MoreNavigationBar.m
+++ DailyReview/src/MoreNavigationBar.m
@@ -2,15 +2,6 @@
 
 @implementation MoreNavigationBar
 
-- (id)init;
-{
-  if ((self = [super init]))
-  {
-  }
-
-  return self;
-}
-
 - (void)drawRect:(CGRect)rect {
 	UIImage *image = [UIImage imageNamed:@"bottom-bar-bg"];
 	[image drawInRect:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
